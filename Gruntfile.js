c38a5d9974e679f2d3676594d2d914b6fb2da03a
jnmarcus <jnmarcus872@gmail.com>
2014-03-29 21:58:06 -0500

    updates to grunt to use only packages that I need

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,81 +1,48 @@
-/*global module:false*/
 module.exports = function(grunt) {
+  'use strict';
+
+  // Load grunt tasks automatically
+  require('matchdep').filterDev('grunt-!(cli)').forEach(grunt.loadNpmTasks);
 
   // Project configuration.
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+
     // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
+    less: {
+      noMinify: {
+        options: {
+          sourceMap: true,
+          sourceMapFilename: 'custom-bootstrap.map'
+        },
+        files: {
+          'dist/css/custom-bootstrap.css': ['less/custom-bootstrap.less'],
+        },
       },
-      dist: {
-        src: ['lib/<%= pkg.name %>.js'],
-        dest: 'dist/<%= pkg.name %>.js'
+      minify: {
+        options: {
+          cleancss: true,
+        },
+        files: {
+          'dist/css/custom-bootstrap.min.css': ['less/custom-bootstrap.less'],
+        },
       }
     },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
+
     watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
+      all: {
+        files: ['less/**/*.less'],
+        tasks: ['less'],
+        options: {
+          spawn: false,
+        },
       },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
-      }
-    }
+    },
   });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
+  //TASKS
   // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('default', ['less', 'watch']);
 
 };
